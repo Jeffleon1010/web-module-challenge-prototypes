7b43ebc404e67270ee1e64854e206bdf1b8ef4f2
Jeff Aranguren <jeffaranguren@Jeffs-iMac.attlocal.net>
2022-04-20 19:31:14 -0400

    finished

--- index.js
+++ index.js
@@ -15,8 +15,23 @@
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+  this.stomach = [];
+}
 
+Person.prototype.eat = function(someFood) {
+  if (this.stomach.length < 10) {
+    this.stomach.push(someFood);
+  }
+}
+
+Person.prototype.poop = function() {
+  this.stomach = [];
+}
+Person.prototype.toString = function() {
+  return `${this.name}, ${this.age}`
 }
 
 
@@ -36,8 +51,15 @@ function Person() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
+function Car(model, milesPerGallon) {
+  this.model = model;
+  this.milesPerGallon = milesPerGallon;
+  this.tank = 0;
+  this.odometer = 0;
+}
 
+Car.prototype.fill = function(gallons) {
+  this.tank += gallons;
 }
 
 
@@ -49,18 +71,23 @@ function Car() {
         + Should return a string "Playing with x", x being the favorite toy.
 */
 
-function Baby() {
-
+function Baby(name, age, favoriteToy) {
+  Person.call(this, name, age);
+  this.favoriteToy = favoriteToy;
 }
 
+Baby.prototype = Object.create(Person.prototype);
+Baby.prototype.play = function (){
+  return `Playing with ${this.favoriteToy}`
+}
 
 /* 
   TASK 4
   In your own words explain the four principles for the "this" keyword below:
-  1. 
-  2. 
-  3. 
-  4. 
+  1. Window binding = When no other rules apply, the global object or undefined will be returned
+  2. Implicit binding = When function is invoked, this refers to what is on the left of the dot
+  3. Explicit binding = .call, .apply, .bind explicitly binds what the keywords are
+  4. New binding = When functions are created as a constructor this points to the new object
 */
 
 ///////// END OF CHALLENGE /////////
